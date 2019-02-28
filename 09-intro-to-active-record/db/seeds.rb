2725b62f7d9d0d7a2c73b61076b06c8d039b2785
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-02-28 14:50:02 -0500

    post lecture 09

--- 09-intro-to-active-record/db/seeds.rb
+++ 09-intro-to-active-record/db/seeds.rb
@@ -1,6 +1,6 @@
 require 'require_all'
 require_relative '../config/environment.rb'
-require_relative '../lib/author.rb'
+require_all 'lib'
 
 Author.find_or_create_by(name: "Mark Twain")
 Author.find_or_create_by(name: "Charles Dickens")
--- 09-intro-to-active-record/lib/author.rb
+++ 09-intro-to-active-record/lib/author.rb
@@ -1,3 +1,9 @@
+require_relative '../config/environment'
+
 class Author < ActiveRecord::Base
-    
+
+    def say_hello_mod_1
+        puts "'Sup"
+    end
+
 end
