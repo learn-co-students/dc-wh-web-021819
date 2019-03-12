b07e278ee15801bf9c5185a370ffccd973c16fb7
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-03-12 12:41:16 -0400

    post lecture 14

--- 14-sinatra-forms/app/controllers/application_controller.rb
+++ 14-sinatra-forms/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
 
     set :views, "app/views"
+    set :method_override, true
 
     get "/" do
         @gems = ["Pearl", "Amethyst", "Garnet"]
@@ -21,9 +22,43 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
 
+    get "/books/new" do
+        erb :new
+    end
+
     get "/books/:id" do 
         book_id = params[:id]
         @book = Book.find(book_id)
         erb :show
     end
+
+    post "/books" do
+        author = params[:author]
+        title = params[:title]
+        snippet = params[:snippet]
+        book = Book.create(author: author, title: title, snippet: snippet)
+        redirect "books/#{book.id}"
+    end
+
+    get "/books/:id/edit" do
+        @book = Book.find(params[:id])
+        erb :edit
+    end
+
+    put "/books/:id" do
+        book = Book.find(params[:id])
+        byebug
+        book.update(author: params[:author],
+                     title: params[:title],
+                     snippet: params[:snippet])
+        redirect "books/#{book.id}"
+    end
+
+    delete "/books/:id" do 
+        book = Book.find(params[:id])
+        book.delete
+        redirect "/books"
+        # erb :index
+    end
+
 end
