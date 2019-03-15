ea58852e993bf1a89b712eb38de0f17c93017062
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-03-15 14:40:40 -0400

    post lecture 17

--- 17-rails-crud/snacktracker/app/controllers/snacks_controller.rb
+++ 17-rails-crud/snacktracker/app/controllers/snacks_controller.rb
@@ -1,11 +1,12 @@
 class SnacksController < ApplicationController
 
+    before_action :get_snack, only: [:show, :update, :edit, :destroy]
+
   def index
     @snacks = Snack.all
   end
 
   def show
-    @snack = Snack.find(params[:id])
   end
 
   def home
@@ -22,10 +23,30 @@ class SnacksController < ApplicationController
     redirect_to @snack
   end
 
+  def edit
+  end
+
+  def update
+    @snack.update(snack_params)
+    redirect_to snack_path(@snack)
+  end
+
+  def destroy
+    @snack.delete
+    redirect_to snacks_path
+  end
+
+  def heart
+  end
+
   private
 
   def snack_params
+    # strong params
     params.require(:snack).permit(:name, :deliciousness, :calories)
   end
 
+  def get_snack
+    @snack = Snack.find(params[:id])
+  end
 end
--- 17-rails-crud/snacktracker/config/routes.rb
+++ 17-rails-crud/snacktracker/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get "/awesome_snacks", to: "snacks#index", as: "snacks"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :snacks, only: [:index, :show, :new, :create]
   root "snacks#home"
+  resources :snacks
+  get "/i-heart-snacks", to: "snacks#heart", as: "heart"
 end
